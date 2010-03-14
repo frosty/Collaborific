8561149aded308697d361bb8d4631c5859fc837e
James Frost <james@jameswfrost.co.uk>
2010-03-14 16:32:29 +0000

    Fixed story model. Models should now all be working correctly, but need validation rules adding.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ActiveRecord::Base
   has_many :fics
   has_many :users, :through => :fics
-  belongs_to :owner, :class_name => "User", :foreign_key => :user_id
+  belongs_to :owner, :class_name => "User", :foreign_key => "owner"
 end
