549ec79f1b89a8259ccdd1beb3b8c1c99585aa9c
Sean Handley <sean.handley@gmail.com>
2010-03-19 18:40:19 +0000

    Forced user names to be appended to each fic when shown in the feed.

--- app/controllers/fics_controller.rb
+++ app/controllers/fics_controller.rb
@@ -30,6 +30,10 @@ class FicsController < ApplicationController
   
   end
   
+  def rss_content
+    @fic.content + "<br><br>Written by: #{@fic.user}"
+  end
+  
 private
 
   def find_story
--- app/models/fic.rb
+++ app/models/fic.rb
@@ -3,4 +3,8 @@ class Fic < ActiveRecord::Base
   belongs_to :story
   
   validates_presence_of :content, :message => "is blank"
+  
+  def rss_content
+    content + "<br/><br/>Written by: #{user.login}"
+  end
 end
