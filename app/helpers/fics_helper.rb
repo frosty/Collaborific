9152a6df199b4e03a484305f71a75b0a869ecaa1
James Frost <james@jameswfrost.co.uk>
2010-03-20 22:58:11 +0000

    Removed a stray exclamation mark.

--- app/helpers/fics_helper.rb
+++ app/helpers/fics_helper.rb
@@ -2,6 +2,6 @@ module FicsHelper
   def wrap_in_paragraphs(content)
     split_content = content.split("\r\n\r\n")
     split_content.map! {|c| "<p>#{c}</p>\n"}
-    split_content.join("").gsub!("\r\n","<br/>\n")
+    split_content.join("").gsub("\r\n","<br/>\n")
   end
 end
