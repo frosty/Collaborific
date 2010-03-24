0fb32fbcb24fe6834f1fe78b8965a95c2a79d8d2
James Frost <james@jameswfrost.co.uk>
2010-03-24 13:22:43 +0000

    Installed Machinist plugin and added unit tests for Fics.

--- test/unit/fic_test.rb
+++ test/unit/fic_test.rb
@@ -1,8 +1,38 @@
 require 'test_helper'
 
 class FicTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :all
+  
+  should_belong_to :user
+  should_belong_to :story
+  
+  test "should validate presence of content" do
+    assert_raise (ActiveRecord::RecordInvalid) {
+      @fic = Fic.make(:content => nil)
+    }
+    #assert_match(/must be shorter than \d+ words./, @fic.errors.on(:content))
+  end
+  
+  test "should validate content length when enforce is enabled" do
+    assert_raise (ActiveRecord::RecordInvalid) {
+      fic = Fic.make(:content => "Three words long", 
+                     :story => Story.make(:fic_length => 2, 
+                                           :fic_length_enforce => true))
+    }
+    #assert_equal("must be shorter than 2 words.", @fic.errors.on(:content))
+  end
+  
+  test "should not validate content length when enforce is disabled" do
+    assert_nothing_raised (ActiveRecord::RecordInvalid) {
+      @fic = Fic.make(:content => "Three words long", 
+                     :story => Story.make(:fic_length => 2, 
+                                           :fic_length_enforce => false))
+    }
+    assert @fic.save
+  end
+  
+  test "should save a valid fic" do
+    fic = Fic.make
+    assert fic.save
   end
 end
